68fb8038dd3e9811246e8602d26196541753e283
Logamurugan <lpilavad@in.ibm.com>
2019-07-01 10:25:35 +0530

    Update git_tools.py

--- git_tools.py
+++ git_tools.py
@@ -1,4 +1,4 @@
-def commentGitPR(prnumber, comment):
+def commentGitPRUsingCurl(prnumber, comment):
     data = {'body': comment}
     user = 'your_git_account'
     token = 'your_git_token'
@@ -8,5 +8,16 @@ def commentGitPR(prnumber, comment):
 
     curlProcess = ["curl", "-i", "-H", f"Authorization: token {token}", "-X", "POST", url, '--data', json.dumps(data)]
     subprocess.run(curlProcess, stdout=subprocess.PIPE, check=True)
+    
+def commentGitPRUsingRequests(prnumber, comment):
+    data = {'body': comment}
+    user = 'your_git_account'
+    token = 'your_git_token'
+    repo_org = 'your_repo_org'
+    repo = 'your_repo'
+    url = 'https://api.github.com/' + f'repos/{repo_org}/{repo}/issues/' + str(prnumber) + '/comments'
 
-commentGitPR(12345, 'Congratz! your PR has passed the tests :tada:')
+    res = requests.post(url, auth=(user,token), data=json.dumps(data))
+    
+
+commentGitPRUsingCurl(12345, 'Congratz! your PR has passed the tests :tada:')
